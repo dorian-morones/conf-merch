b08bff60025c4d23539cde303a88f054e7cbdd7f
Dorian Morones <dorianm393@gmail.com>
2020-12-29 22:03:02 -0800

    Checkout page - styles

--- src/views/Checkout/index.jsx
+++ src/views/Checkout/index.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import {
+  CheckoutContainer,
+  Item,
+  Element,
+  Sidebar,
+  SidebarButton,
+  Delete,
+  Title,
+  Text,
+} from "./styles/checkout";
 
 const Checkout = () => {
-  return <p>checkout</p>;
+  console.log("checkout");
+
+  return (
+    <CheckoutContainer>
+      <div className="Checkout-content">
+        <h3>Lista de Pedidos:</h3>
+        <Item>
+          <Element>
+            <Title>ITEM name</Title>
+            <Text>$10</Text>
+          </Element>
+          <Delete type="button">Eliminar</Delete>
+        </Item>
+      </div>
+      <Sidebar>
+        <h3>Precio Total: $10</h3>
+        <SidebarButton type="button">Continuar pedido</SidebarButton>
+      </Sidebar>
+    </CheckoutContainer>
+  );
 };
 
 export default Checkout;
