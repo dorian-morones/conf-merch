9bcade52181b4977449f6a352b81e6bbe0585db1
Dorian Morones <dorianm393@gmail.com>
2020-12-29 22:41:44 -0800

    Success View

--- src/views/Success/index.jsx
+++ src/views/Success/index.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { SuccessContainer, SuccessMap } from "./styles/success";
 
-const Sucess = () => {
-  return <p>sucess</p>;
+const Success = () => {
+  return (
+    <SuccessContainer>
+      <div className="Success-content">
+        <h2>Dorian, Gracias por tu compra</h2>
+        <span>Tu pedido lelgara en 3 dias a tu direccion:</span>
+        <SuccessMap>Google Maps</SuccessMap>
+      </div>
+    </SuccessContainer>
+  );
 };
 
-export default Sucess;
+export default Success;
---
+++ src/views/Success/styles/success.js
@@ -0,0 +1,10 @@
+/* eslint-disable import/prefer-default-export */
+import styled from "styled-components";
+
+export const SuccessContainer = styled.div`
+  margin: 0 0 4em 0;
+`;
+
+export const SuccessMap = styled.div`
+  margin: 10px 0 0 0;
+`;
